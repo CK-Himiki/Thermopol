4d7a31b20b8b3c866c8b56ba25abfb97f3919015
Astra-chan-desu <lyosha5438@gmail.com>
2024-05-26 06:01:51 +0300

    Frontend main commit
    Пытаемся пытаться.

--- myapp/static/myscr/index.js
+++ myapp/static/myscr/index.js
@@ -1,23 +1,48 @@
-let mn=document.querySelector('#nvb-menu');
-
-async function ovrlShow(){
-	let wn = document.querySelector('.overlay');
-	if (wn.style.display=="block"){
-		setTimeout(function(){;}, 500);
-		if (await wn.animate({opacity: '0.0'}, 200).finished) {wn.style.opacity='0.0';wn.style.display = "";}
-		console.log(document.querySelector(".nav-btn").replaceChildren(lll));
-		
-	}
-	else {
-		wn.style.display = "block";
-		if (await wn.animate({opacity: '0.5'}, 200).finished) {wn.style.opacity='0.5';}
-		console.log(document.querySelector(".nav-btn").replaceChildren(x));
-		
-	}
-}
+document.addEventListener('DOMContentLoaded', function () {
+    //получим все scrollbox
 
 
-let x =document.createElement('div');
-x.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"  viewBox="0 0 50 50"> <path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path>';
-let lll= document.createElement('div');
-lll.innerHTML = '<span class="icon-bar" style="background-color: black; float: right ;"></span> <span class="icon-bar" style="background-color: black;	float: right ;"></span> <span class="icon-bar" style="background-color: black;	float: right ;"></span>'
+
+    
+    const scrollingWrapper = document.querySelectorAll('.scrollingBlock');
+    scrollingWrapper.forEach(scrollingBlock => {
+        //активация первой ссылки сразу при создании 
+         document.querySelectorAll(scrollingBlock.getAttribute('scrl') +' .index-link')[0].classList.add('active');
+        scrollingBlock.addEventListener('scroll', function () {
+            //кружочек который надо
+            let currentIndex = -1;
+            let elcount = this.children.length;
+          
+            //элемемент относительно видимой области
+            currentIndex = ((this.scrollLeft+10) / parseFloat(getComputedStyle(this.querySelector(".item")).width));
+            if (currentIndex> (elcount-1.5)) { currentIndex = elcount-1;}
+            else {currentIndex = Math.floor(currentIndex)}
+
+            if (currentIndex !== -1) {
+                document.querySelectorAll(scrollingBlock.getAttribute('scrl') +' .index-link').forEach(link => link.classList.remove('active'));
+                document.querySelectorAll(scrollingBlock.getAttribute('scrl') +' .index-link')[currentIndex].classList.add('active');
+
+                }
+            });
+        });
+
+
+
+     const navlinck = document.querySelectorAll('#nvb-menu a');
+    navlinck.forEach(link => {
+        link.addEventListener("click",
+
+        function(){
+            document.querySelector('.nav-btn').click();
+        });
+
+
+        }
+        )
+
+
+           
+        }
+
+    
+);
