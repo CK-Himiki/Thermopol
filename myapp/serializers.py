2854374865d171c74b1fa5c264f9081b27296da8
Aleksandr Kulikov <unnotigkeit@yandex.ru>
2024-06-05 13:36:20 +0300

    add serializers

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from .models import Substance
-from .models import Source
+from .models import *
+
 
 class SubstanceSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,9 +8,31 @@ class SubstanceSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-
-
 class SourceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Source
+        fields = '__all__'
+
+
+class StateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = State
+        fields = '__all__'
+
+
+class PhaseTransitionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhaseTransition
+        fields = '__all__'
+
+
+class TransitionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transition
+        fields = '__all__'
+
+
+class PhaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Phase
         fields = '__all__'
